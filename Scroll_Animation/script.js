0cfb683d9a2e192847e7d08d4ef80a6fc80d60c3
KeWa <kewa@kewa.io>
2023-11-12 19:34:09 -0600

    Added JavaScript

--- Scroll_Animation/script.js
+++ Scroll_Animation/script.js
@@ -1,1 +1,13 @@
 const box = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+
+  checkBoxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect();
+  });
+}
