6875d3157d1d2bb5340aae545aecfc0c2c913e02
KeWa <kewa@kewa.io>
2023-11-16 19:52:42 -0600

    Added JS to project

--- Form_Wave_Animation/script.js
+++ Form_Wave_Animation/script.js
@@ -1,1 +1,11 @@
-Vanilla_JS_Projects / Starter_Files / script.js;
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach((label) => {
+  label.innerHTML = label.innerText
+    .split('')
+    .map(
+      (letter, idx) =>
+        `<span style='transition-delay:${idx * 40}ms'>${letter}</span>`
+    )
+    .join('');
+});
