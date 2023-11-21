a4f243b13c391bbf39f0d718a25f66347ef3e43f
KeWa <kewa@kewa.io>
2023-11-20 22:23:17 -0600

    Added JS to project

--- Dad_Jokes/script.js
+++ Dad_Jokes/script.js
@@ -1,1 +1,20 @@
+const joke = document.getElementById('joke');
+const jokeBtn = document.getElementById('jokeBtn');
 
+jokeBtn.addEventListener('click', generateJoke);
+
+generateJoke();
+
+function generateJoke() {
+  const config = {
+    headers: {
+      accept: 'application/json',
+    },
+  };
+
+  fetch('https://icanhazdadjoke.com/', config)
+    .then((res) => res.json())
+    .then((data) => {
+      joke.innerHTML = data.joke;
+    });
+}
