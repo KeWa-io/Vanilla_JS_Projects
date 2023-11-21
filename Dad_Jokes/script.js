954522a1d836cad2d8e611d8d209640bbab13577
KeWa <kewa@kewa.io>
2023-11-20 22:28:24 -0600

    Updated JS file

--- Dad_Jokes/script.js
+++ Dad_Jokes/script.js
@@ -5,16 +5,32 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
 
-function generateJoke() {
+//Using .then
+// function generateJoke() {
+//   const config = {
+//     headers: {
+//       accept: 'application/json',
+//     },
+//   };
+
+//   fetch('https://icanhazdadjoke.com/', config)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       joke.innerHTML = data.joke;
+//     });
+// }
+
+//Using Async/Await
+async function generateJoke() {
   const config = {
     headers: {
       accept: 'application/json',
     },
   };
 
-  fetch('https://icanhazdadjoke.com/', config)
-    .then((res) => res.json())
-    .then((data) => {
-      joke.innerHTML = data.joke;
-    });
+  const res = await fetch('https://icanhazdadjoke.com/', config);
+
+  const data = await res.json();
+
+  joke.innerHTML = data.joke;
 }
